4824f0990e9666a177eb870fc72a60a53126aca5
Ubuntu <ubuntu@ip-172-31-42-240.us-east-2.compute.internal>
2019-10-26 02:22:26 +0000

    wip

--- self_health_check/config.py
+++ self_health_check/config.py
@@ -1,4 +1,4 @@
 import os
 
-SLACK_URL = os.getenv('SLACK_URL', '')
+SLACK_URL = os.getenv('SLACK_URL', 'https://hooks.slack.com/services/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')
 BLACKBOX_EXPORTER_URL = 'http://localhost:9115'
