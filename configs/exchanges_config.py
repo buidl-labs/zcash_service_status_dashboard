f01c7f5c89f9c2f4fd9ff5853bd54e5e3d57bb46
Suryansh Singh <suryansh.80@gmail.com>
2020-02-17 02:33:35 +0530

    changed exchange config file

--- configs/exchanges_config.py
+++ configs/exchanges_config.py
@@ -21,7 +21,14 @@ EXMO_USD_SPOT_PRICE_DEVIATION_GAUGE = Gauge('exmo_usd_spot_price_deviation_gauge
                                             'exmo_usd_spot_price_deviation_gauge gauge')
 EXMO_BTC_SPOT_PRICE_DEVIATION_GAUGE = Gauge('exmo_btc_spot_price_deviation_gauge',
                                             'exmo_btc_spot_price_deviation_gauge gauge')
-
+EXMO_USD_SPOT_PRICE_ABSOLUTE = Gauge(
+    'exmo_usd_spot_price_absolute', 'exmo_usd_spot_price_absolute_gauge')
+EXMO_BTC_SPOT_PRICE_ABSOLUTE = Gauge(
+    'exmo_btc_spot_price_absolute', 'exmo_btc_spot_price_absolute_gauge')
+EXMO_USD_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'exmo_usd_transaction_volume_absolute', 'exmo_usd_transaction_volume_absolute_gauge')
+EXMO_BTC_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'exmo_btc_transaction_volume_absolute', 'exmo_btc_transaction_volume_absolute_gauge')
 
 # Bitlish Configs
 BITLISH_URL = "https://bitlish.com/api/v1/tickers"
@@ -39,7 +46,14 @@ BITLISH_USD_SPOT_PRICE_DEVIATION_GAUGE = Gauge('bitlish_usd_spot_price_deviation
                                                'bitlish_usd_spot_price_deviation_gauge gauge')
 BITLISH_BTC_SPOT_PRICE_DEVIATION_GAUGE = Gauge('bitlish_btc_spot_price_deviation_gauge',
                                                'bitlish_btc_spot_price_deviation_gauge gauge')
-
+BITLISH_USD_SPOT_PRICE_ABSOLUTE = Gauge(
+    'bitlish_usd_spot_price_absolute', 'bitlish_usd_spot_price_absolute_gauge')
+BITLISH_BTC_SPOT_PRICE_ABSOLUTE = Gauge(
+    'bitlish_btc_spot_price_absolute', 'bitlish_btc_spot_price_absolute_gauge')
+BITLISH_USD_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'bitlish_usd_transaction_volume_absolute', 'bitlish_usd_transaction_volume_absolute_gauge')
+BITLISH_BTC_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'bitlish_btc_transaction_volume_absolute', 'bitlish_btc_transaction_volume_absolute_gauge')
 
 # Bittrex Configs
 BITTREX_ZEC_USD_SPOT_PRICE_URL = "https://api.bittrex.com/api/v1.1/public/getticker?market=USD-ZEC"
@@ -59,7 +73,14 @@ BITTREX_USD_SPOT_PRICE_DEVIATION_GAUGE = Gauge('bittrex_usd_spot_price_deviation
                                                'bittrex_usd_spot_price_deviation_gauge gauge')
 BITTREX_BTC_SPOT_PRICE_DEVIATION_GAUGE = Gauge('bittrex_btc_spot_price_deviation_gauge',
                                                'bittrex_btc_spot_price_deviation_gauge gauge')
-
+BITTREX_USD_SPOT_PRICE_ABSOLUTE = Gauge(
+    'bittrex_usd_spot_price_absolute', 'bittrex_usd_spot_price_absolute_gauge')
+BITTREX_BTC_SPOT_PRICE_ABSOLUTE = Gauge(
+    'bittrex_btc_spot_price_absolute', 'bittrex_btc_spot_price_absolute_gauge')
+BITTREX_USD_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'bittrex_usd_transaction_volume_absolute', 'bittrex_usd_transaction_volume_absolute_gauge')
+BITTREX_BTC_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'bittrex_btc_transaction_volume_absolute', 'bittrex_btc_transaction_volume_absolute_gauge')
 
 # Gemini Configs
 GEMINI_ZEC_BTC_URL = "https://api.gemini.com/v1/pubticker/zecbtc"
@@ -78,6 +99,14 @@ GEMINI_USD_SPOT_PRICE_DEVIATION_GAUGE = Gauge('gemini_usd_spot_price_deviation_g
                                               'gemini_usd_spot_price_deviation_gauge gauge')
 GEMINI_BTC_SPOT_PRICE_DEVIATION_GAUGE = Gauge('gemini_btc_spot_price_deviation_gauge',
                                               'gemini_btc_spot_price_deviation_gauge gauge')
+GEMINI_USD_SPOT_PRICE_ABSOLUTE = Gauge(
+    'gemini_usd_spot_price_absolute', 'gemini_usd_spot_price_absolute_gauge')
+GEMINI_BTC_SPOT_PRICE_ABSOLUTE = Gauge(
+    'gemini_btc_spot_price_absolute', 'gemini_btc_spot_price_absolute_gauge')
+GEMINI_USD_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'gemini_usd_transaction_volume_absolute', 'gemini_usd_transaction_volume_absolute_gauge')
+GEMINI_BTC_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'gemini_btc_transaction_volume_absolute', 'gemini_btc_transaction_volume_absolute_gauge')
 
 # Bitfinex Configs
 BITFINEX_ZEC_USD_URL = "https://api-pub.bitfinex.com/v2/tickers?symbols=tZECUSD"
@@ -96,6 +125,14 @@ BITFINEX_USD_SPOT_PRICE_DEVIATION_GAUGE = Gauge('bitfinex_usd_spot_price_deviati
                                                 'bitfinex_usd_spot_price_deviation_gauge gauge')
 BITFINEX_BTC_SPOT_PRICE_DEVIATION_GAUGE = Gauge('bitfinex_btc_spot_price_deviation_gauge',
                                                 'bitfinex_btc_spot_price_deviation_gauge gauge')
+BITFINEX_USD_SPOT_PRICE_ABSOLUTE = Gauge(
+    'bitfinex_usd_spot_price_absolute', 'bitfinex_usd_spot_price_absolute_gauge')
+BITFINEX_BTC_SPOT_PRICE_ABSOLUTE = Gauge(
+    'bitfinex_btc_spot_price_absolute', 'bitfinex_btc_spot_price_absolute_gauge')
+BITFINEX_USD_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'bitfinex_usd_transaction_volume_absolute', 'bitfinex_usd_transaction_volume_absolute_gauge')
+BITFINEX_BTC_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'bitfinex_btc_transaction_volume_absolute', 'bitfinex_btc_transaction_volume_absolute_gauge')
 
 # Binance Configs
 # TODO: Figure out ZEC<->USD as Binance does not provide any pair that includes USD
@@ -108,6 +145,10 @@ BINANCE_SPOT_PRICE_MEDIAN_DEVIATION_PORT = Histogram(
     'binance_spot_price_deviation_check', 'Binance Spot Price Deviation Check')
 BINANCE_BTC_SPOT_PRICE_DEVIATION_GAUGE = Gauge('binance_btc_spot_price_deviation_gauge',
                                                'binance_btc_spot_price_deviation_gauge gauge')
+BINANCE_BTC_SPOT_PRICE_ABSOLUTE = Gauge(
+    'binance_btc_spot_price_absolute', 'binance_btc_spot_price_absolute_gauge')
+BINANCE_BTC_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'binance_btc_transaction_volume_absolute', 'binance_btc_transaction_volume_absolute_gauge')
 
 # Coinbase Configs
 # TODO: Figure out ZEC<->BTC as Coinbase does not provide this pair.
@@ -116,6 +157,8 @@ COINBASE_SPOT_PRICE_USD_PORT = Enum('coinbase_spot_price_usd_check',
                                     'Coinbase Spot Price USD Check', states=['1', '0'])
 COINBASE_USD_SPOT_PRICE_DEVIATION_GAUGE = Gauge('coinbase_usd_spot_price_deviation_gauge',
                                                 'coinbase_usd_spot_price_deviation_gauge gauge')
+COINBASE_USD_SPOT_PRICE_ABSOLUTE = Gauge(
+    'coinbase_usd_spot_price_absolute', 'coinbase_usd_spot_price_absolute_gauge')
 
 # Kraken Configs
 KRAKEN_ZEC_USD_URL = "https://api.kraken.com/0/public/Ticker?pair=ZECUSD"
@@ -125,6 +168,10 @@ KRAKEN_TRANSACTION_VOLUME_USD_PORT = Enum(
     'kraken_transaction_volume_usd_check', 'Kraken Transaction Volume USD Check', states=['1', '0'])
 KRAKEN_USD_SPOT_PRICE_DEVIATION_GAUGE = Gauge('kraken_usd_spot_price_deviation_gauge',
                                               'kraken_usd_spot_price_deviation_gauge gauge')                                # Gauge Configs for Spot Price Deviation
+KRAKEN_USD_SPOT_PRICE_ABSOLUTE = Gauge(
+    'kraken_usd_spot_price_absolute', 'kraken_usd_spot_price_absolute_gauge')
+KRAKEN_USD_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'kraken_usd_transaction_volume_absolute', 'kraken_usd_transaction_volume_absolute_gauge')
 
 # Coinjar Configs
 COINJAR_ZEC_BTC_URL = "https://data.exchange.coinjar.com/products/ZECBTC/ticker"
@@ -134,6 +181,12 @@ COINJAR_TRANSACTION_VOLUME_BTC_PORT = Enum(
     'coinjar_transaction_volume_btc_check', 'Coinjar Transaction Volume BTC Check', states=['1', '0'])
 COINJAR_BTC_SPOT_PRICE_DEVIATION_GAUGE = Gauge('coinjar_btc_spot_price_deviation_gauge',
                                                'coinjar_btc_spot_price_deviation_gauge gauge')
+COINJAR_BTC_SPOT_PRICE_ABSOLUTE = Gauge(
+    'coinjar_btc_spot_price_absolute', 'coinjar_btc_spot_price_absolute_gauge')
+COINJAR_BTC_TRANSACTION_VOLUME_ABSOLUTE = Gauge(
+    'coinjar_btc_transaction_volume_absolute', 'coinjar_btc_transaction_volume_absolute_gauge')
+
+# Universal configs
 
 USD_EXCHANGE = {
     "Exmo": EXMO_USD_SPOT_PRICE_DEVIATION_GAUGE,
@@ -154,3 +207,47 @@ BTC_EXCHANGE = {
     "Binance": BINANCE_BTC_SPOT_PRICE_DEVIATION_GAUGE,
     "Coinjar": COINJAR_BTC_SPOT_PRICE_DEVIATION_GAUGE
 }
+
+ABSOLUTE_PRICE_USD_EXCHANGE = {
+    "Exmo": EXMO_USD_SPOT_PRICE_ABSOLUTE,
+    "Bitlish": BITLISH_USD_SPOT_PRICE_ABSOLUTE,
+    "Bittrex": BITTREX_USD_SPOT_PRICE_ABSOLUTE,
+    "Gemini": GEMINI_USD_SPOT_PRICE_ABSOLUTE,
+    "Bitfinex": BITFINEX_USD_SPOT_PRICE_ABSOLUTE,
+    "Coinbase": COINBASE_USD_SPOT_PRICE_ABSOLUTE,
+    "Kraken": KRAKEN_USD_SPOT_PRICE_ABSOLUTE
+}
+
+ABSOLUTE_PRICE_BTC_EXCHANGE = {
+    "Exmo": EXMO_BTC_SPOT_PRICE_ABSOLUTE,
+    "Bitlish": BITLISH_BTC_SPOT_PRICE_ABSOLUTE,
+    "Bittrex": BITTREX_BTC_SPOT_PRICE_ABSOLUTE,
+    "Gemini": GEMINI_BTC_SPOT_PRICE_ABSOLUTE,
+    "Bitfinex": BITFINEX_BTC_SPOT_PRICE_ABSOLUTE,
+    "Binance": BINANCE_BTC_SPOT_PRICE_ABSOLUTE,
+    "Coinjar": COINJAR_BTC_SPOT_PRICE_ABSOLUTE
+}
+
+ABSOLUTE_TRANSACTION_VOLUME_USD_EXCHANGE = {
+    "Exmo": EXMO_USD_TRANSACTION_VOLUME_ABSOLUTE,
+    "Bitlish": BITLISH_USD_TRANSACTION_VOLUME_ABSOLUTE,
+    "Bittrex": BITTREX_USD_TRANSACTION_VOLUME_ABSOLUTE,
+    "Gemini": GEMINI_USD_TRANSACTION_VOLUME_ABSOLUTE,
+    "Bitfinex": BITFINEX_USD_TRANSACTION_VOLUME_ABSOLUTE,
+    "Kraken": KRAKEN_USD_TRANSACTION_VOLUME_ABSOLUTE
+}
+
+ABSOLUTE_TRANSACTION_VOLUME_BTC_EXCHANGE = {
+    "Exmo": EXMO_BTC_TRANSACTION_VOLUME_ABSOLUTE,
+    "Bitlish": BITLISH_BTC_TRANSACTION_VOLUME_ABSOLUTE,
+    "Bittrex": BITTREX_BTC_TRANSACTION_VOLUME_ABSOLUTE,
+    "Gemini": GEMINI_BTC_TRANSACTION_VOLUME_ABSOLUTE,
+    "Bitfinex": BITFINEX_BTC_TRANSACTION_VOLUME_ABSOLUTE,
+    "Binance": BINANCE_BTC_TRANSACTION_VOLUME_ABSOLUTE,
+    "Coinjar": COINJAR_BTC_TRANSACTION_VOLUME_ABSOLUTE
+}
+
+MEDIAN_SPOT_PRICE_USD = Gauge(
+    'median_spot_price_usd', 'median_spot_price_usd_gauge')
+MEDIAN_SPOT_PRICE_BTC = Gauge(
+    'median_spot_price_btc', 'median_spot_price_btc_gauge')
