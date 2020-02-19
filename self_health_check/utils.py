a043ebbd7783da517e8421a248164757e92bc11e
Suryansh Singh <suryansh.80@gmail.com>
2020-02-19 23:40:36 +0530

    made changes for monitoring

--- self_health_check/utils.py
+++ self_health_check/utils.py
@@ -16,3 +16,15 @@ def send_slack_notification(message):
         return response
     except:
         print("Error in posting to slack")
+
+def send_slack_notification_urgent(message):
+    url = SLACK_URL_URGENT
+    headers = {'Content-Type': 'application/json'}
+    data = {"text": message}
+    try:
+        response = requests.post(url, data=json.dumps(
+        data), headers=headers, timeout=20)
+        # Unsure whether the message was posted!
+        return response
+    except:
+        print("Error in posting to slack")
